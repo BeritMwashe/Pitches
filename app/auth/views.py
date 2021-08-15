c047e2182cb4cb311be46cf11bd1020d9e9a7523
Amukozoberit <mwasheb@gmail.com>
2021-08-15 13:55:25 +0300

    added logout route

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,30 @@
+import email
+from urllib import request
+
+from flask_login import login_required, login_user, logout_user
+from ..models import User
 from .forms import LoginForm
-from flask import render_template
+from flask import render_template,redirect,request,url_for,flash
 from . import auth
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
     form=LoginForm()
+    if form.validate_on_submit():
+        user=User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user,form.remember_me.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+       
     return render_template('authtemplates/login.html',form=form)
+
+
+
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+   
+
